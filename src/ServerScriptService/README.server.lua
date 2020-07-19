cd672be968690d46d0b81910f576178e0498cf5c
Chipio Industries <chipioindustries@gmail.com>
2020-07-19 11:06:50 -0700

    initial commit

---
+++ src/ServerScriptService/README.server.lua
@@ -0,0 +1,13 @@
+--[[------------------------------------
+	WRITTEN BY: ChipioIndustries
+	DATE CREATED: 7/19/2020
+	DATE EDITED: 7/19/2020
+
+INFO
+
+This is a simple project designed to showcase
+my ability to create well-structured, properly
+documented games. It's also a good chance to
+practice Roact/Rodux.
+
+--]]------------------------------------
